8a70ebbd606bf4662eafd9920291945d5a09199d
“leodrom” <dmitry.kosaryev@gmail.com>
2021-11-29 01:08:01 +0200

    Numbering

--- scripts.js
+++ scripts.js
@@ -1,7 +1,45 @@
-const cards = document.querySelectorAll('.memory-card');
+cards = document.querySelectorAll('.memory-card');
+
+
+function shuffelCards()
+{
+  cards = document.querySelectorAll('.memory-card');
+  
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * (cards.length - 2)) + 1;
+
+    let exchangeHTML = cards[0].innerHTML;
+    cards[0].innerHTML = cards[randomPos].innerHTML;
+    
+    cards[randomPos].innerHTML = exchangeHTML;
+  
+  })
+}
 
 function flipCard() {
   this.classList.toggle('flip');
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+function setCardsListener(){
+  cards = document.querySelectorAll('.memory-card');
+
+  cards.forEach(card => {
+    card.addEventListener('click', flipCard);
+  })
+}
+
+
+function setCardsNumber()
+{
+  cards = document.querySelectorAll('.memory-card');
+  
+  cards.forEach((card, index) => {
+    paragraph = card.querySelector('.back-face-fnt');
+    paragraph.innerHTML = index + 1;
+  })
+}
+
+shuffelCards();
+setCardsListener();
+setCardsNumber();
+
